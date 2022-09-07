0d28627904e5ea06b1c76bb6e07cc0867d2e8865
Katherine <liponina.katya@yandex.ru>
2022-09-07 10:39:26 +0300

    step 4

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -6,9 +6,9 @@ public class Cli {
     public static String getPlayerName() {
         System.out.print("May I know your name? ");
         Scanner scanner = new Scanner(System.in);
-        String PlayerName = scanner.next();
+        String playerName = scanner.next();
         scanner.close();
-        return PlayerName;
+        return playerName;
     }
 
 }
